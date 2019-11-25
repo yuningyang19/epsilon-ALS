00a36d90bf7ce2322f06b1d7db88d150b524c194
yuningyang19 <51691872+yuningyang19@users.noreply.github.com>
2019-11-25 16:35:51 +0800

    updated

---
+++ alg/diff_U_Uout.m
@@ -0,0 +1,8 @@
+function [ norm_diff ] = diff_U_Uout( U, Uout )
+%DIFF_U_UOUT Summary of this function goes here
+%   Detailed explanation goes here
+norm_diff = sqrt(sum(cellfun(@(u,v)(u(:)-v(:))'* ...
+        (u(:)-v(:)),U,Uout)))/sqrt(sum(cellfun(@(u)u(:)'*u(:),U)));
+
+end
+
