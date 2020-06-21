47e58514475490be319a4b03e74f37ae7dc8eedd
yuningyang19 <51691872+yuningyang19@users.noreply.github.com>
2020-06-21 17:20:13 +0800

    updated

--- utils/column_matching.m
+++ utils/column_matching.m
@@ -8,10 +8,15 @@ function [ Uout ] = column_matching( U,Uhat )
 decision_mat = abs(decision_mat); % because ambiguity of the sign, we need only 
 % the absolute values of the decision matrix.
 
-[max_v,place] = max(decision_mat'); % max takes the maximum value of each column. Thus 
+if verLessThan('matlab','9.7') % version less than Matlab 2019 a simplified H algorithm is used.
+[~,place] = max(decision_mat'); % max takes the maximum value of each column. Thus 
 % we need to transpose the decision matrix.
 % place records the true place that the columns of Uhat should appear.
 % for example, if place= [2 1 3], then it maps [1 2 3] -> [2 1 3];
+else 
+   [place,~] = matchpairs(decision_mat',-1,'max');  % version above Matlab 2019, use the built-in function.
+   place = place(:,1);
+end
 
 % put the largest entry of each row of decision_mat to 1, and other to 0,
 % then it becoms a permutation matrix. Then Uout = Uhat*decision_mat.
