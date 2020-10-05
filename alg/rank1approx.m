1b33fa84648e9bde66c48a2cb98c677786fb15df
yuningyang19 <51691872+yuningyang19@users.noreply.github.com>
2020-10-05 19:49:41 +0800

    bug fixed

--- alg/rank1approx.m
+++ alg/rank1approx.m
@@ -11,18 +11,18 @@ if d == 1
     end
 elseif d == 2
     if size(B,1) == 1
-        x{1} = B/norm(B(:));
-    elseif size(B,2) == 1
         x{1} = B'/norm(B(:));
+    elseif size(B,2) == 1
+        x{1} = B/norm(B(:));
     else
-        [x{1},l,x{2}] = svds(B,1,'L');
+        [x{1},~,x{2}] = svds(B,1,'L');
     end
 else
 %     mB_col_sz = prod(sz)/(sz(end)*sz(end-1));
 %     mB_row_sz = sz(end)*sz(end-1);
 %     mB = reshape(B,mB_col_sz,mB_row_sz);
     mB = tens2mat(B,1:d-2,d-1:d);
-    [~,l,x_mminus1_m] = svds(mB,1,'L');
+    [~,~,x_mminus1_m] = svds(mB,1,'L');
     Xx_mminus1_m = reshape(x_mminus1_m,sz(end-1),sz(end)); %step 4
     tuple_x_mminus1_m = rank1approx(Xx_mminus1_m);
     x_mminus_1 = tuple_x_mminus1_m{1}; x_m = tuple_x_mminus1_m{2};
